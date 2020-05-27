c5a890175034803b78e7e465fb2698af6de41fb5
Ilnur Garifullin <ilnur.email@gmail.com>
2020-05-27 15:17:03 +0300

    Refactor codebase and add Helm charts (#4)
    * Revert "Replaced Postgres with a lighter Sqlite database"
    
    This reverts commit 5357022bef0065c7180edf43b66f9f749177b28d.
    
    * Refactor codebase to be more consise and readable
    
    * Add helm charts
    
    * Add ingress configuration

--- src/log.js
+++ src/log.js
@@ -1,8 +1,9 @@
 const winston = require('winston');
 const { createLogger, transports, format } = winston;
 
+const logLevel = (process.env.LOG_LEVEL || 'debug').toLowerCase();
 const logger = createLogger({
-  level: 'info',
+  level: logLevel,
   format: format.combine(
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
@@ -13,26 +14,23 @@ const logger = createLogger({
   ),
   defaultMeta: { service: 'gitter-forwarder' },
   transports: [
-    //
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
-    //
     new transports.File({ filename: 'gitter-forwarder-error.log', level: 'error' }),
     new transports.File({ filename: 'gitter-forwarder-combined.log' })
   ]
 });
 
-//
 // If we're not in production then **ALSO** log to the `console`
 // with the colorized simple format.
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.simple()
-    )
-  }));
-}
+logger.add(new transports.Console({
+  format: format.combine(
+    format.colorize(),
+    format.simple()
+  )
+}));
 
-module.exports = logger;
+module.exports = {
+  logger: logger,
+  logLevel: logLevel,
+};
